e624aed48c2376211c63909f5f2509138fd6824b
Nahidctg <164381234+Nahidctg@users.noreply.github.com>
2024-03-29 14:10:02 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone Nahidctg/Ctgbot /Nahidctg/Ctgbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /Nahidctg/Ctgbot
 fi
-cd /Advance-Auto-Filter
+cd /Nahidctg/Ctgbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
